e6b18707a3764f1650de4b96c0a8f5e122d1614b
unknown <lohith.duos@gmail.com>
2020-10-15 09:03:06 +0530

    Updates

---
+++ precache-manifest.138bf6b0201d7974def1e19155c1b0f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593e8091b00235325f1988e12eb9f0fe",
+    "url": "/redux-sample-login/index.html"
+  },
+  {
+    "revision": "2c5fc6b648e180800a40",
+    "url": "/redux-sample-login/static/js/2.084f6bc4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/redux-sample-login/static/js/2.084f6bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4bfee7074bb5b6dd2f4",
+    "url": "/redux-sample-login/static/js/main.66a6e49e.chunk.js"
+  },
+  {
+    "revision": "a8989576a5a32c0a931c",
+    "url": "/redux-sample-login/static/js/runtime-main.85ec1b60.js"
+  }
+]);
